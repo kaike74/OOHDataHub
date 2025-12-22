1dc1144c2523ea2213858e0f065542d28d8bdb10
kaike74 <kaike@hubradios.com>
2025-12-22 20:52:40 -0300

    Delete page.tsx

--- frontend/app/propostas/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import Dashboard from '@/components/Dashboard';
-
-export default function ProposalPage({ params }: { params: { id: string } }) {
-    return <Dashboard initialProposalId={params.id} />;
-}
