e8dca3fb3ef031bee0cef6e4b4eda88ec56103e1
kaike74 <kaike@hubradios.com>
2025-12-30 20:40:54 -0300

    Revert "a"
    This reverts commit cb05c13134096d2cc41684f48ef73a3cfcb20573.

--- frontend/components/layout/TopBar.tsx
+++ frontend/components/layout/TopBar.tsx
@@ -38,7 +38,7 @@ export default function TopBar({ breadcrumbs, user, actions, counts }: TopBarPro
                         <img
                             src="/assets/logoHorizontalAzul.png"
                             alt="E-Mídias"
-                            className="h-12 w-auto object-contain transition-transform hover:scale-105"
+                            className="h-9 w-auto object-contain transition-transform hover:scale-105"
                         />
                     </Link>
 
